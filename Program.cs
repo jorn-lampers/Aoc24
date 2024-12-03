1bdcf871bce3614e0760bb14836c8dc73ed02688
Jorn Lampers <jornlampers97@gmail.com>
2024-12-03 20:33:18 +0100

    Day 3

--- Program.cs
+++ Program.cs
@@ -3,35 +3,103 @@ using System.Collections.Generic;
 
 // Using linq
 using System.Linq;
+using System.Text.RegularExpressions;
 
-string[] lines = File.ReadAllLines("input1");
+Day3p2();
 
-List<int> leftList = lines
-  .Select(l => int.Parse(l.Split(" ")[0]))
-  .OrderBy(n => n)
-  .ToList();
+void Day1() {
+  string[] lines = File.ReadAllLines("input1");
 
-List<int> rightList = lines
-  .Select(l => int.Parse(l.Split(" ").Where(s => s.Length > 0).ToArray()[1]))
-  .OrderBy(n => n)
-  .ToList();
+  List<int> leftList = lines
+    .Select(l => int.Parse(l.Split(" ")[0]))
+    .OrderBy(n => n)
+    .ToList();
 
-int totalDistance = 0;
-for (int i = 0; i < leftList.Count; i++)
-{
-  int distance = Math.Abs(leftList[i] - rightList[i]);
-  totalDistance += distance;
+  List<int> rightList = lines
+    .Select(l => int.Parse(l.Split(" ").Where(s => s.Length > 0).ToArray()[1]))
+    .OrderBy(n => n)
+    .ToList();
+
+  int totalDistance = 0;
+  for (int i = 0; i < leftList.Count; i++)
+  {
+    int distance = Math.Abs(leftList[i] - rightList[i]);
+    totalDistance += distance;
+  }
+
+  Console.WriteLine($"Total distance: {totalDistance}");
+
+  var totalSimilarityScore = 0;
+  for (int i = 0; i < leftList.Count; i++)
+  {
+    int amountOfOccurencesInRightList = rightList.Count(n => n == leftList[i]);
+    int similarityScore = amountOfOccurencesInRightList * leftList[i];
+
+    totalSimilarityScore += similarityScore;
+  }
+
+  Console.WriteLine($"Total similarity score: {totalSimilarityScore}");
 }
 
-Console.WriteLine($"Total distance: {totalDistance}");
+void Day3p1() {
+  string input = File.ReadAllText("input3");
 
-var totalSimilarityScore = 0;
-for (int i = 0; i < leftList.Count; i++)
-{
-  int amountOfOccurencesInRightList = rightList.Count(n => n == leftList[i]);
-  int similarityScore = amountOfOccurencesInRightList * leftList[i];
+  // regex match mul\(\d+,\d+\)
+  var regex = new Regex(@"mul\(\d+,\d+\)");
+  
+  var matches = regex.Matches(input);
 
-  totalSimilarityScore += similarityScore;
+  int total = 0;
+  foreach (var match in matches)
+  {
+
+    total += ResolveMul(match.ToString());
+  }
+
+  Console.WriteLine($"Sum of products: {total}");
 }
 
-Console.WriteLine($"Total similarity score: {totalSimilarityScore}");
+void Day3p2() {
+  string input = File.ReadAllText("input3");
+
+  string doParts = FindDoParts(input);
+
+  var regex = new Regex(@"mul\(\d+,\d+\)");
+  var matches = regex.Matches(doParts);
+
+  int total = 0;
+  foreach (var match in matches)
+  {
+    total += ResolveMul(match.ToString());
+  }
+
+  Console.WriteLine($"Sum of products: {total}");
+}
+
+string FindDoParts(string input) {
+
+  List<string> doParts = new List<string>();
+
+  int startIndex = 0;
+  while (startIndex != -1)
+  {
+    int endOfDo = input.IndexOf("don't()", startIndex);
+    if (endOfDo == -1)
+    {
+      endOfDo = input.Length;
+    }
+
+    doParts.Add(input.Substring(startIndex, endOfDo - startIndex));
+
+    startIndex = input.IndexOf("do(", endOfDo);
+  }
+
+  return doParts.Aggregate((a, b) => a + b);
+}
+
+int ResolveMul(string input) {
+  int n1 = int.Parse(input.Split(",")[0].Replace("mul(", ""));
+  int n2 = int.Parse(input.Split(",")[1].Replace(")", ""));
+
+  return n1 * n2;
+}
